8c94f099bb134f6fecef7daa6a31f68139934a66
Ridhsuki <basukiridhoalghifary@gmail.com>
2025-02-27 17:42:21 +0700

    Feat: CRUD products API (get, edit, delete product)
    1. ProductController.php
        public function index()
        {
            $products = Product::all();
            return response()->json($products);
        }
    
        public function show($id)
        {
            $product = Product::find($id);
            if(!$product) {
                return response()->json([
                    'message' => 'product not found',
                    'status' => 'error',
                ]);
            }
            return response()->json($product);
        }
    
        public function update(UpdateProductRequest $request, $id)
        {
            $product = Product::find($id);
    
            $product->name = $request->name;
            $product->price = $request->price;
            $product->description = $request->description;
            $product->stock = $request->stock;
            $product->favorite = $request->favorite;
            $product->status = $request->status;
            $product->save();
    
            if ($request->file('image')) {
                $image = $request->file('image');
                $image->storeAs('public/products', $product->id . '.png');
                $product->image = $product->id . '.png';
                $product->save();
            }
    
            return response()->json([
                'status' => true,
                'message' => 'update product successfully'
            ], 200);
        }
    
        public function destroy($id)
        {
            $product = Product::find($id);
    
            $product->delete();
    
            return response()->json([
                'status' => true,
                'message' => 'delete product successfully'
            ], 200);
        }
    
    2. UpdateProductRequest.php
        public function authorize(): bool
        {
            return Auth::check();
        }
    
    3. Test di Postman Post, get, put, del

--- eticket-laravel-api/app/Http/Controllers/ProductController.php
+++ eticket-laravel-api/app/Http/Controllers/ProductController.php
@@ -13,7 +13,8 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        $products = Product::all();
+        return response()->json($products);
     }
 
     /**
@@ -62,9 +63,16 @@ class ProductController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Product $product)
+    public function show($id)
     {
-        //
+        $product = Product::find($id);
+        if(!$product) {
+            return response()->json([
+                'message' => 'product not found',
+                'status' => 'error',
+            ]);
+        }
+        return response()->json($product);
     }
 
     /**
@@ -78,16 +86,43 @@ class ProductController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateProductRequest $request, Product $product)
+    public function update(UpdateProductRequest $request, $id)
     {
-        //
+        $product = Product::find($id);
+
+        $product->name = $request->name;
+        $product->price = $request->price;
+        $product->description = $request->description;
+        $product->stock = $request->stock;
+        $product->favorite = $request->favorite;
+        $product->status = $request->status;
+        $product->save();
+
+        if ($request->file('image')) {
+            $image = $request->file('image');
+            $image->storeAs('public/products', $product->id . '.png');
+            $product->image = $product->id . '.png';
+            $product->save();
+        }
+
+        return response()->json([
+            'status' => true,
+            'message' => 'update product successfully'
+        ], 200);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Product $product)
+    public function destroy($id)
     {
-        //
+        $product = Product::find($id);
+
+        $product->delete();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'delete product successfully'
+        ], 200);
     }
 }
